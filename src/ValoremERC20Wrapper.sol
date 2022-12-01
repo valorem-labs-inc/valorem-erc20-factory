c00a6de40657105b783e0bdf8cf421c86b1cd4fd
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2022-12-01 02:29:58 -0500

    add default solmate ERC20 inheritance

--- src/ValoremERC20Wrapper.sol
+++ src/ValoremERC20Wrapper.sol
@@ -2,22 +2,26 @@
 pragma solidity 0.8.11;
 
 import "./interfaces/IValoremERC20Wrapper.sol";
+import "solmate/tokens/ERC20.sol";
 
-contract ValoremERC20Wrapper is IValoremERC20Wrapper {
+contract ValoremERC20Wrapper is IValoremERC20Wrapper, ERC20 {
     /// @notice The address of the valorem core option settelement contract.
     address public valoremCore;
 
     
     /*//////////////////////////////////////////////////////////////
-    //  Constructor
+    // constructor 
     //////////////////////////////////////////////////////////////*/
 
-    /// @notice ValoremERC20Factory constructor
+    /// @notice ValoremERC20Wrapper initializer 
     /// @param valoremCoreOptionSettlementEngine Core contract address
+    /// @param optionId The id of the valorem core option type.
+    /// @param option True if this ERC20 is to wrap fungible ERC115s from the
+    /// core contract, False if this ERC20 is to wrap nonfungible claim NFTs.
     constructor(
         address valoremCoreOptionSettlementEngine,
         uint160 optionId, 
-        bool option) 
+        bool option) ERC20("", "", 0)
     {
         valoremCore = valoremCoreOptionSettlementEngine;
     }

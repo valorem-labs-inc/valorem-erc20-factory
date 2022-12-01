e6996143ecc2c1dad97309d853fe7b7618032a49
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2022-12-01 02:30:51 -0500

    lint!

--- src/ValoremERC20Wrapper.sol
+++ src/ValoremERC20Wrapper.sol
@@ -8,21 +8,16 @@ contract ValoremERC20Wrapper is IValoremERC20Wrapper, ERC20 {
     /// @notice The address of the valorem core option settelement contract.
     address public valoremCore;
 
-    
     /*//////////////////////////////////////////////////////////////
     // constructor 
     //////////////////////////////////////////////////////////////*/
 
-    /// @notice ValoremERC20Wrapper initializer 
+    /// @notice ValoremERC20Wrapper initializer
     /// @param valoremCoreOptionSettlementEngine Core contract address
     /// @param optionId The id of the valorem core option type.
     /// @param option True if this ERC20 is to wrap fungible ERC115s from the
     /// core contract, False if this ERC20 is to wrap nonfungible claim NFTs.
-    constructor(
-        address valoremCoreOptionSettlementEngine,
-        uint160 optionId, 
-        bool option) ERC20("", "", 0)
-    {
+    constructor(address valoremCoreOptionSettlementEngine, uint160 optionId, bool option) ERC20("", "", 0) {
         valoremCore = valoremCoreOptionSettlementEngine;
     }
 

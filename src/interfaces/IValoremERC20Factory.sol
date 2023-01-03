535a43acb8f7e450d4cbb27a65b04e401e1e8522
Flip <13227294+Flip-Liquid@users.noreply.github.com>
2023-01-03 07:33:53 -0600

    update compiler

--- src/interfaces/IValoremERC20Factory.sol
+++ src/interfaces/IValoremERC20Factory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL 1.1
-pragma solidity 0.8.11;
+pragma solidity 0.8.16;
 
 /**
  * This defines the interface for an ERC20 wrapper factory for Valorem Core nonfungible and
